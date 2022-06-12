30a6df611e7d23a9bc6d1201bd1f1e12a7023702
Ryfd55 <gubardin1984@mail.ru>
2022-06-13 00:15:32 +0400

    task_4 (take)

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,7 +4,15 @@ fun main() {
 
 
 
-//                      task_3     MAP
+
+//                      task_4     Бесконечная последовательность + take
+//    val array = generateSequence(1) { it + 1 }
+//    val list = array.take(100).map { "Сотрудник №$it" }
+//    for (i in list)
+//        println(i)
+
+
+//                      task_3     MAP метод
 //    val randomCollect = mutableListOf<Int>()
 //    for (i in 0..99) {
 //        randomCollect.add(Random.nextInt(0, 1000))
@@ -18,6 +26,7 @@ fun main() {
 //    var oldList = mutableListOf(232, 34, 45, 3, 35, 2)
 //    val res = sort(oldList)
 //    println(res)
+
 
 //                      task_1     FILTER метод
 //    val listOfNames = mutableListOf("Alex", "Bob", "Antony")
