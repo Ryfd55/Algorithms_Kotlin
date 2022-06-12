24825646e1ddecb245dabd885f33ec93deb5f9e9
Ryfd55 <gubardin1984@mail.ru>
2022-06-12 21:29:44 +0400

    task_3 (map)

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,14 +1,28 @@
+import kotlin.random.Random
+
 fun main() {
 
 
-//                      task_1     ДЕФОЛТНЫЕ ПАРАМЕТРЫ
-//   println  (volume(2,3))
+
+//                      task_3     MAP
+//    val randomCollect = mutableListOf<Int>()
+//    for (i in 0..99) {
+//        randomCollect.add(Random.nextInt(0, 1000))
+//    }
+//    val collect1 = randomCollect.filter { it % 3 == 0 || it % 5 == 0 }.map { it * it }.sortedDescending().toString()
+//    println(randomCollect)
+//    println(collect1)
+
 
 //                      task_2     ПУЗЫРЬКОВАЯ СОРТИРОВКА
 //    var oldList = mutableListOf(232, 34, 45, 3, 35, 2)
 //    val res = sort(oldList)
 //    println(res)
 
+//                      task_1     FILTER метод
+//    val listOfNames = mutableListOf("Alex", "Bob", "Antony")
+//    val listOfANames = listOfNames.filter { it.first() == 'A' }
+//    println(listOfANames)
 
 }
 
