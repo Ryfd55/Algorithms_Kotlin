5273daf433842f4804415fae28ffdc0f04fa0c79
Ryfd55 <gubardin1984@mail.ru>
2022-06-12 17:02:20 +0400

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+
+//                      task_1     ДЕФОЛТНЫЕ ПАРАМЕТРЫ
+//   println  (volume(2,3))
+
+//                      task_2     ПУЗЫРЬКОВАЯ СОРТИРОВКА
+//    var oldList = mutableListOf(232, 34, 45, 3, 35, 2)
+//    val res = sort(oldList)
+//    println(res)
+
+
+}
+
+
+
---
+++ src/main/kotlin/task_2.kt
@@ -0,0 +1,15 @@
+//                       ПУЗЫРЬКОВАЯ СОРТИРОВКА
+fun sort(oldList: MutableList<Int>): List<Int> {
+    var tmp: Int
+    for (j in 0 until oldList.size - 1) {
+        for (i in 0 until (oldList.size - 1 - j)) {
+            if (oldList[i] > oldList[i + 1]) {
+                tmp = oldList[i]
+                oldList[i] = oldList[i + 1]
+                oldList[i + 1] = tmp
+            }
+        }
+        println(oldList)
+    }
+    return oldList
+}
