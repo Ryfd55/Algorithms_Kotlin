14ae756bcaf172cb3865ee74b0533be7758c0234
Ryfd55 <gubardin1984@mail.ru>
2022-06-22 16:37:11 +0300

    task_16

--- src/main/kotlin/task_16.kt
+++ src/main/kotlin/task_16.kt
@@ -1,3 +1,17 @@
-fun fib(n: Int): Int {
-    for (n in 1..n)
+fun fib(n: Int): List<Int> {
+    var x =0
+    var x1 = 1
+    var x2 = 1
+    val fibArray = mutableListOf<Int>()
+    for (i in 1..n){
+        fibArray.add(x)
+        x = x1
+    x1=x2
+    x2=x+x1
+
+        }
+
+    return fibArray
+
+
 }
